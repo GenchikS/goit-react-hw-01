d989771870f29dc5642003755001d652010d8333
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-08-07 19:38:44 +0300

    v1.1.2(+task-1)

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+// завдання 1
+// import Profile from "./profile/profile"
+// import userData from "./userData.json"
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Profile
+//         image={userData.avatar}
+//         name = {userData.username}
+//         tag={userData.tag}
+//         location={userData.location}
+//         stats={userData.stats}
+//       />
+//     </div>
+//   )
+// }
+
+
+//  завдання 2
+// export default function AppFriend() {
+//   return (
+//     <div>
+//       <FriendList/>
+//     </div>
+//   )
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './components/App.jsx'
+import Product from "./components/product/Product.jsx"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App/>
+  // <App />
+  <Product/>
   )
+
